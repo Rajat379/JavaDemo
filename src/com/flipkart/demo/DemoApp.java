d12772496468da9885b643b3a5ae6efc17bfa587
Rajat Garg <rajat.garg@BLRETV-Y40Y60XC.local>
2022-07-22 10:05:55 +0530

    First Commit

---
+++ src/com/flipkart/demo/DemoApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author rajat.garg
+ *
+ */
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
